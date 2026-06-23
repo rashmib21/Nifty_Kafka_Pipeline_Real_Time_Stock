497a59277ff14e7d6ece52c4b547f498a943e7b2
rashmib21 <rashmibarethiya2130@gmail.com>
2026-06-23 14:59:30 +0530

    add kafka settigs

--- nifty/config.py
+++ nifty/config.py
@@ -9,3 +9,8 @@ CLIENT_ID=os.getenv('CLIENT_ID','')
 PASSWORD=os.getenv('PASSWORD','')
 TOTP_SECRET=os.getenv('TOTP_SECRET','')
 
+#kafka settings
+KAFKA_BROKER='localhost:9092'
+KAKFA_TOPIC='stock-ticks'
+KAFKA_DLQ='stock-dlq'
+KAFKA_GROUP_ID='stock-consumers'
