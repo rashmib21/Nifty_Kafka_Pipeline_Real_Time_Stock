e704c15274bb1cccbc686ef5383ae1d6d23e4548
rashmib21 <rashmibarethiya2130@gmail.com>
2026-06-23 14:53:40 +0530

    add credentials

--- nifty/config.py
+++ nifty/config.py
@@ -2,3 +2,10 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
+#Angel One credentials, read from .env
+API_KEY=os.getenv('API_KEY','')
+CLIENT_ID=os.getenv('CLIENT_ID','')
+PASSWORD=os.getenv('PASSWORD','')
+TOTP_SECRET=os.getenv('TOTP_SECRET','')
+
