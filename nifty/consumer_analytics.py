46813af969fb8e9881039e7de4670e1e7412c7d6
rashmib21 <rashmibarethiya2130@gmail.com>
2026-06-29 16:33:10 +0530

    define indian standard time and initialize empty dictionary to keep running totals

--- nifty/consumer_analytics.py
+++ nifty/consumer_analytics.py
@@ -5,3 +5,25 @@ from datetime import datetime
 from kafka import KafkaConsumer
 import mysql.connector
 
+from config import (
+    KAFKA_BROKER, KAFKA_TOPIC,
+    DB_HOST, DB_USER, DB_PASSWORD, DB_NAME,
+    MARKET_OPEN, MARKET_CLOSE
+)
+
+IST = pytz.timezone('Asia/Kolkata')
+
+#Dictionary to keep running totals for each stock in memory,We need these to calculate VWAP correctly
+#running_totals = {
+#     'RELIANCE': {
+#         'total_price_x_volume': 5000000,
+#         'total_volume': 1500,
+#         'tick_count': 200,
+#         'day_high': 2900.0,
+#         'day_low': 2800.0
+#     }
+# }
+
+running_totals = {}
+
+
