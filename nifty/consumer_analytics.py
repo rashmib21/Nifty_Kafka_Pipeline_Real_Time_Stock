9ddcb0b72a6775ada5784842a78853babbf0a406
rashmib21 <rashmibarethiya2130@gmail.com>
2026-06-29 16:40:04 +0530

    define Kafka Consumer

--- nifty/consumer_analytics.py
+++ nifty/consumer_analytics.py
@@ -39,3 +39,14 @@ def is_market_open():
         return False
 
 
+kafka_consumer = KafkaConsumer(
+    KAFKA_TOPIC,
+    bootstrap_servers  = [KAFKA_BROKER],
+    group_id           = 'analytics-consumer-group',
+    auto_offset_reset  = 'earliest',
+    enable_auto_commit = False,
+    isolation_level    = 'read_committed',
+    value_deserializer = lambda v: json.loads(v.decode('utf-8'))
+)
+         
+
