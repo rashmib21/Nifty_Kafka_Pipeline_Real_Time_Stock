7a96a5fae26f1da6d3c9ee2f348819c4e6a95a0b
rashmib21 <rashmibarethiya2130@gmail.com>
2026-07-01 12:19:47 +0530

    add kafka consumer congiuration

--- nifty/common.py
+++ nifty/common.py
@@ -3,3 +3,14 @@ import json
 
 def deserializer(v):
     return json.loads(v.decode('utf-8'))
+
+KAFKA_CONSUMER_CONFIG = {
+    'bootstrap_servers'   : [KAFKA_BROKER],
+    'auto_offset_reset'   : 'earliest',
+    'enable_auto_commit'  : False,
+    'isolation_level'     : 'read_committed',
+    'value_deserializer'  : deserializer,
+    'session_timeout_ms'  : 30000,     
+    'heartbeat_interval_ms': 10000,    
+    'max_poll_interval_ms' : 300000    
+}
